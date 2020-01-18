39241dd6c798572d1eae0da906f1675058d653fc
sunchao09091 <sunchao09091@hellobike.com>
2020-01-18 10:19:28 +0800

    3

---
+++ 3-从尾到头打印链表.java
@@ -0,0 +1,97 @@
+//栈实现
+/**
+*    public class ListNode {
+*        int val;
+*        ListNode next = null;
+*
+*        ListNode(int val) {
+*            this.val = val;
+*        }
+*    }
+*
+*/
+import java.util.*;
+public class Solution {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> res = new ArrayList<>();
+        if(listNode == null) return res;
+        while(listNode != null){
+            stack.push(listNode.val);
+            listNode = listNode.next;
+        }
+        while(!stack.isEmpty()){
+            res.add(stack.pop());
+        }
+        return res;
+    }
+}
+
+
+
+//递归实现
+/**
+*    public class ListNode {
+*        int val;
+*        ListNode next = null;
+*
+*        ListNode(int val) {
+*            this.val = val;
+*        }
+*    }
+*
+*/
+import java.util.ArrayList;
+public class Solution {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if(listNode == null) return res;
+        res.addAll(printListFromTailToHead(listNode.next));
+        res.add(listNode.val);
+        return res;
+    }
+}
+
+
+
+
+//头插法实现
+/**
+*    public class ListNode {
+*        int val;
+*        ListNode next = null;
+*
+*        ListNode(int val) {
+*            this.val = val;
+*        }
+*    }
+*
+*/
+import java.util.ArrayList;
+public class Solution {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if(listNode == null) return res;
+        ListNode dummy = new ListNode(0);
+        dummy.next = null;
+        ListNode cur = listNode;
+        while(cur != null){
+            ListNode next = cur.next;
+            cur.next = dummy.next;
+            dummy.next = cur;
+            cur = next;
+        }
+        cur = dummy.next;
+        while(cur != null){
+            res.add(cur.val);
+            cur = cur.next;
+        }
+        return res;
+    }
+}
+
+
+
+
+
+
