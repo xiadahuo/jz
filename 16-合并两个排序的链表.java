d9451a74be53d99df24375522ad9584075bf1821
sunchao09091 <sunchao09091@hellobike.com>
2020-01-28 09:28:47 +0800

    modify 16

--- 16-合并两个排序的链表.java
+++ 16-合并两个排序的链表.java
@@ -31,3 +31,29 @@ public class Solution {
         return dummy.next;
     }
 }
+
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+//递归
+public class Solution {
+    public ListNode Merge(ListNode list1,ListNode list2) {
+        if(list1 == null) return list2;
+        else if(list2 == null) return list1;
+        ListNode head = null;
+        if(list1.val < list2.val){
+            head = list1;
+            head.next = Merge(list1.next, list2);
+        }else{
+            head = list2;
+            head.next = Merge(list1, list2.next);
+        }
+        return head;
+    }
+}
