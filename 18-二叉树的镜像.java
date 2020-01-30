4aff2c2633664916a38a8381c7e7ee0d00bfb7c9
sunchao09091 <sunchao09091@hellobike.com>
2020-01-30 11:37:51 +0800

    modify-18

--- 18-二叉树的镜像.java
+++ 18-二叉树的镜像.java
@@ -54,3 +54,39 @@ public class Solution {
         root.right = temp;
     }
 }
+
+
+//先序非递归
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+import java.util.*;
+public class Solution {
+    public void Mirror(TreeNode root) {
+        if(root == null) return;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while(cur != null || !stack.isEmpty()){
+            if(cur != null){
+                TreeNode help = cur.left;
+                cur.left = cur.right;
+                cur.right = help;
+                stack.push(cur);
+                cur = cur.left;
+            }else{
+                TreeNode temp = stack.pop();
+                cur = temp.right;
+            }
+        }
+    }
+}
