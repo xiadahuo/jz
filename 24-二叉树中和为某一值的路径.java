4cccfe83c132a97748a2852b6d08fdb7879bd245
sunchao09091 <sunchao09091@hellobike.com>
2020-02-05 18:00:49 +0800

    modify-24-2

--- 24-二叉树中和为某一值的路径.java
+++ 24-二叉树中和为某一值的路径.java
@@ -72,3 +72,49 @@ public class Solution {
         temp.remove(temp.size()-1);
     }
 }
+
+
+
+import java.util.ArrayList;
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public ArrayList<ArrayList<Integer>> FindPath(TreeNode root,int target) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        helper(root, target, res, new ArrayList<>());
+        return res;
+    }
+    public void helper(TreeNode root, int target, ArrayList<ArrayList<Integer>> res,
+                       ArrayList<Integer> temp){
+        if(root == null) return;
+        if(target < root.val) return;  //优化剪枝
+        temp.add(root.val);
+        if(root.left == null && root.right == null && root.val == target){
+            res.add(new ArrayList<>(temp));
+        }else{
+            helper(root.left, target-root.val, res, temp);
+            helper(root.right, target-root.val, res, temp);
+        }
+        temp.remove(temp.size()-1);
+    }
+}
+
+
+
+
+
+
+
+
+
