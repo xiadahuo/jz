089782c8f8105b036dfc2eb5f50387834ed41b52
sunchao09091 <sunchao09091@hellobike.com>
2020-02-06 11:20:43 +0800

    modify-25

--- 25-复杂链表的复制.java
+++ 25-复杂链表的复制.java
@@ -14,21 +14,21 @@ public class Solution {
     {
         if(pHead == null) return null;
         RandomListNode cur = pHead;
-        while(cur != null){
+        while(cur != null){ //复制每个节点的值到它后面
             RandomListNode clone = new RandomListNode(cur.label);
             clone.next = cur.next;
             cur.next = clone;
             cur = cur.next.next;
         }
         cur = pHead;
-        while(cur != null){
+        while(cur != null){ //修改复制的节点的random指针
             if(cur.random != null)
                 cur.next.random = cur.random.next;
             cur = cur.next.next;
         }
         RandomListNode cloneHead = pHead.next;
         cur = pHead;
-        while(cur != null){
+        while(cur != null){ //分离原链表与复制的链表
             RandomListNode next = cur.next;
             if(next != null){
                 cur.next = next.next;
