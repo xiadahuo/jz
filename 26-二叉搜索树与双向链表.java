0817db3e54673ee51a9eac93e16bb7ec28a2a1bd
sunchao09091 <sunchao09091@hellobike.com>
2020-02-07 17:47:54 +0800

    26-v2

--- 26-二叉搜索树与双向链表.java
+++ 26-二叉搜索树与双向链表.java
@@ -63,4 +63,45 @@ public class Solution {
 }
 
 
+//非递归形式
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+import java.util.*;
+public class Solution {
+    private TreeNode head = null;
+    private TreeNode pre = null;
+    public TreeNode Convert(TreeNode pRootOfTree) {
+        inOrder(pRootOfTree);
+        return head;
+    }
+    public void inOrder(TreeNode root){
+        if(root == null) return;
+        Stack<TreeNode> stack = new Stack<>();
+        while(!stack.isEmpty() || root != null){
+            while(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+            TreeNode cur = stack.pop();
+            if(head == null) head = cur;
+            cur.left = pre;
+            if(pre != null) pre.right = cur;
+            pre = cur;
+            root = cur.right;
+        }
+    }
+}
+
+
 
