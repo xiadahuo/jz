5ef592fb35c391ae111381c29079ae217465e0ba
sunchao09091 <sunchao09091@hellobike.com>
2020-02-08 22:39:43 +0800

    27

--- 27-字符串的排列.java
+++ 27-字符串的排列.java
@@ -26,3 +26,36 @@ public class Solution {
         }
     }
 }
+
+//法二：交换
+import java.util.*;
+public class Solution {
+    public ArrayList<String> Permutation(String str) {
+       ArrayList<String> res = new ArrayList<>();
+        if(str == null || str.length()== 0) return res;
+        char[] c = str.toCharArray();
+        Arrays.sort(c);
+        helper(res, c, 0, str.length());
+        Collections.sort(res);
+        return res;
+    }
+    public void helper(ArrayList<String> res, char[] c, int index, int end){
+        if(index == end){
+            res.add(new String(c));
+        }else{
+            for(int i = index; i < end; i++){
+                if(i == index || c[i] != c[index]){
+                    swap(c,i,index);
+                    helper(res, c, index+1, end);
+                    swap(c,i,index);
+                }
+            }
+        }
+        
+    }
+    public void swap(char[] c, int i, int j){
+        char temp = c[i];
+        c[i] = c[j];
+        c[j] = temp;
+    }
+}
