c1fd24f00f59bbd7782c5e691ee5bd9700cf057f
sunchao09091 <sunchao09091@hellobike.com>
2020-02-12 14:45:37 +0800

    29

--- 29-最小的k个数.java
+++ 29-最小的k个数.java
@@ -60,3 +60,59 @@ public class Solution {
         return res;
     }
 }
+
+
+
+//强行用自己建的堆
+import java.util.*;
+public class Solution {
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if(input == null || input.length == 0 || k <= 0 || k > input.length)
+            return res;
+        if(k == input.length){
+            for(int i = 0; i < input.length; i++)
+                res.add(input[i]);
+            return res;
+        }
+        int[] heap = new int[k+1];
+        for(int i = 0; i <= k; i++){ //初始化
+            heap[i] = input[i];
+        }
+        for(int i = 0; i <= k; i++){ //调整大顶堆，大小为k+1
+            heapInsert(heap,i);
+        }
+        for(int i = k+1; i < input.length; i++){
+            heap[0] = input[i];
+            heapify(heap, 0, k+1);
+        }
+        for(int i = 1; i <= k; i++){
+            res.add(heap[i]);
+        }
+        return res;
+    }
+    public void heapInsert(int[] array, int index){
+        while(array[index] > array[(index-1)/2]){
+            swap(array, index, (index-1)/2);
+            index = (index-1)/2;
+        }
+    }
+    public void heapify(int[] array, int index, int heapSize){
+        int left = index * 2+1;
+        while(left < heapSize){
+            int largest = left+1 < heapSize && array[left+1] > array[left]
+                ? left+1 : left;
+            largest = array[largest] > array[index] ? largest : index;
+            if(largest == index) break;
+            swap(array, largest, index);
+            index = largest;
+            left = index*2+1;
+        }
+    }
+    
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
