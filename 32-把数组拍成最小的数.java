1cb810da7399bb6cc5721e6fa2fe06acf771ff57
sunchao09091 <sunchao09091@hellobike.com>
2020-02-14 10:24:20 +0800

    32

---
+++ 32-把数组拍成最小的数.java
@@ -0,0 +1,43 @@
+//排序完拼接
+import java.util.*;
+public class Solution {
+    public String PrintMinNumber(int [] numbers) {
+    	if(numbers == null || numbers.length == 0) return "";
+        String[] nums = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++){
+            nums[i] = numbers[i] + "";
+        }
+        Arrays.sort(nums, new Comparator<String>(){
+            @Override
+            public int compare(String str1, String str2){
+                String res1 = str1+str2;
+                String res2 = str2+str1;
+                return res1.compareTo(res2);
+            }
+        });
+        StringBuilder sb = new StringBuilder();
+        for(String str : nums){
+            sb.append(str);
+        }
+        return sb.toString();
+    }
+}
+
+
+//lambda表达式版本
+import java.util.*;
+public class Solution {
+    public String PrintMinNumber(int [] numbers) {
+    	if(numbers == null || numbers.length == 0) return "";
+        String[] nums = new String[numbers.length];
+        for(int i = 0; i < numbers.length; i++){
+            nums[i] = numbers[i] + "";
+        }
+        Arrays.sort(nums, (s1,s2)->(s1+s2).compareTo(s2+s1));
+        StringBuilder sb = new StringBuilder();
+        for(String str : nums){
+            sb.append(str);
+        }
+        return sb.toString();
+    }
+}
