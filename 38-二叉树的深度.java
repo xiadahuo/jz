08ce4854ae331d5d08b5e68578b9322a9078b70b
sunchao09091 <sunchao09091@hellobike.com>
2020-02-20 08:12:26 +0800

    38

---
+++ 38-二叉树的深度.java
@@ -0,0 +1,43 @@
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public int TreeDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = TreeDepth(root.left);
+        int right = TreeDepth(root.right);
+        return Math.max(left,right)+1;
+    }
+}
+
+
+
+
+/**
+public class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+
+}
+*/
+public class Solution {
+    public int TreeDepth(TreeNode root) {
+        return root == null ? 0 : Math.max(TreeDepth(root.left),TreeDepth(root.right))+1;
+    }
+}
